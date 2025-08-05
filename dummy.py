bf4d9340423a3d12fda117ce98f701416a94a99d
Sharndeep Kaur <sharnsaini625@gmail.com>
2025-08-05 09:52:09 +0530

    Added the image

--- dummy.py
+++ dummy.py
@@ -15,3 +15,5 @@ selected_country = st.selectbox(
 )
 
 st.write(selected_country)
+
+st.image("https://images.pexels.com/photos/1183099/pexels-photo-1183099.jpeg?_gl=1*ey46ll*_ga*MTI2MDQ2MTM1NS4xNzQ2MDc5Mjc2*_ga_8JE65Q40S6*czE3NTQzNjc2NTUkbzIkZzEkdDE3NTQzNjc2NzAkajQ1JGwwJGgw")
